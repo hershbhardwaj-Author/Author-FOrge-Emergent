f3b566ce07eab9529c8aac06b607d7976268eb2a
hershbhardwaj-Author <hersh.bhardwaj@googlemail.com>
2026-06-03 01:28:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import "@/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ElectricForge from "./ElectricForge";
-import AdminDashboard from "./AdminDashboard";
-import InvestmentPage from "./InvestmentPage";
 import MentorPage from "./MentorPage";
 
-
-// Inside your Routes / Switch:
-<Route path="/mentor" element={<MentorPage />} />
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ElectricForge />} />
-          <Route path="/electric-forge" element={<ElectricForge />} />
-          <Route path="/investment" element={<InvestmentPage />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="*" element={<ElectricForge />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ElectricForge />} />
+        <Route path="/mentor" element={<MentorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
