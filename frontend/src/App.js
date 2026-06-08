2933363507f3dd7d3766bf429850c381fb997346
hershbhardwaj-Author <hersh.bhardwaj@googlemail.com>
2026-06-09 00:55:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ElectricForge from "./ElectricForge";
 import MentorPage from "./MentorPage";
 import InvestmentPage from "./InvestmentPage";
+import ShreemBooksGallery from './ShreemBooksGallery';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<ElectricForge />} />
         <Route path="/mentor" element={<MentorPage />} />
         <Route path="/investment" element={<InvestmentPage />} />
+        <Route path="/shreem-books" element={<ShreemBooksGallery />} />
       </Routes>
     </Router>
   );
